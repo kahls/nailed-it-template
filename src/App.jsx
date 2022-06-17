ee05a8c00ad1b20ce9fcb8cc578434042b39022f
kahls <kyle@kyleahlstrom.com>
2022-06-17 13:36:07 -0500

    putting timer stuff back in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import { Link } from "react-router-dom";
-
+import TimerProvider from "./context/TimerProvider";
+import Timer from "./components/Timer/Timer";
 import BouncyLink from "./components/BouncyLink";
 
 function App() {
   return (
-    <div className="app">
-      <nav className="app-nav">
-        <BouncyLink>
-          <Link to="/">Home</Link>
-        </BouncyLink>
-        <BouncyLink>
-          <Link to="/objective">Objective</Link>
-        </BouncyLink>
-        <BouncyLink>
-          <Link to="/teams">Teams</Link>
-        </BouncyLink>
-        <BouncyLink>
-          <Link to="/thanks">Thank You</Link>
-        </BouncyLink>
-      </nav>
-      <div className="app-content">
-        <Outlet />
+    <TimerProvider>
+      <div className="app">
+        <Timer/>
+        <nav className="app-nav">
+          <BouncyLink>
+            <Link to="/">Home</Link>
+          </BouncyLink>
+          <BouncyLink>
+            <Link to="/objective">Objective</Link>
+          </BouncyLink>
+          <BouncyLink>
+            <Link to="/teams">Teams</Link>
+          </BouncyLink>
+          <BouncyLink>
+            <Link to="/thanks">Thank You</Link>
+          </BouncyLink>
+        </nav>
+        <div className="app-content">
+          <Outlet />
+        </div>
+        <footer>
+          <p>Credera ♥️ XD</p>
+        </footer>
       </div>
-      <footer>
-        <p>Credera ♥️ XD</p>
-      </footer>
-    </div>
+    </TimerProvider>
   );
 }
 
